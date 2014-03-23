5286dc0fac032777daaf483d9f616f93772ce4f8
Daniel Mackay <crankage@gmail.com>
2014-03-23 11:58:19 +1100

    Added implementation.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ngAnimateRepeat
 ===============
 
-AngularJS animation sample using ng-repeate and animate.css.
+AngularJS animation sample using ngAnimate and animate.css for a ng-repeat.  Sliding animations are used when a member is added or removed from the list.  I also use a background animation to highlight new items in the list.
+
+All the animations in this example are done solely via CSS.
+
---
+++ ngAnimateRepeat/scripts/app.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        // Angular modules 
+        'ngAnimate'
+    ]);
+
+    app.controller("MainCtrl", function ($scope) {
+        $scope.count = 0;
+        $scope.items = [$scope.count++ + " Lorem ipsum dolor sit amet, consectetur adipiscing elit"];
+
+        
+        $scope.addAnother = function () {
+            var another = $scope.count++ + " Lorem ipsum dolor sit amet, consectetur adipiscing elit ";
+            $scope.items.splice(1, 0, another);
+        };
+
+        $scope.remove = function () {
+            $scope.items.pop();
+        };
+    });
+})();
